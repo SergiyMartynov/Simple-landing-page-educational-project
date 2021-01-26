0c1eaa75c881d032eedb76f62b0493673fb1c052
MartynovS <sergijmartynov@gmail.com>
2021-01-26 15:10:27 +0200

    change intut serch

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,31 +1,31 @@
-// $(function () {
-//     $('.load-more').on('click', function () {
-//         const btn = $(this);
-//         const loader = btn.find('span');
-//         $.ajax({
-//             url: '/data.html',
-//             type: 'GET',
-//             beforeSend: function () {
-//                 btn.attr('disabled', true);
-//                 loader.addClass('d-inline-block');
-//             },
-//             success: function (responce) {
-//                 setTimeout(function () {
-//                     loader.removeClass('d-inline-block');
-//                     btn.attr('disabled', false);
-//                     $('.after-posts').before(responce)
-//                 }, 1000);
-//             },
-//             error: function () {
-//                 alert('Error!');
-//                 loader.removeClass('d-inline-block');
-//                 btn.attr('disabled', false);
-//             }
-//         });
+//For Search 
 
+const searchBtn = document.querySelector(".search-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const searchBox = document.querySelector(".search-box");
+const searchInput = document.querySelector("input");
+const searchData = document.querySelector(".search-data");
 
-//     });
-// });
-
+searchBtn.onclick = () => {
+    searchBox.classList.add("active");
+    searchInput.classList.add("active");
+    searchBtn.classList.add("active");
+    cancelBtn.classList.add("active");
+    if (searchInput.value != "") {
+        let values = searchInput.value;
+        searchData.classList.remove("active");
+        searchData.innerHTML = "You now enter " + "<span style='font-weight:500;'>" + values + "</span>";
+    } else {
+        searchData.innerHTML = " ";
+    }
+}
+cancelBtn.onclick = () => {
+    searchBox.classList.remove("active");
+    searchInput.classList.remove("active");
+    searchBtn.classList.remove("active");
+    cancelBtn.classList.remove("active");
+    searchData.classList.add("active");
+    searchInput.value = "";
+}
 
 
